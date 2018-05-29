045f884888c7bcd16fc797684cdac6cd6c70d007
sunyuxiao1 <2568479903@qq.com>
2018-05-29 22:48:00 +0800

    添加重置样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './javaScript/rem.js'
+import './index.scss';
 import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
 
